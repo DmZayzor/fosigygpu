903d2a7ed441688c8d34bed01763e843c55e5f6d
Daniel <daarizar@gmail.com>
2021-09-23 06:39:25 -0500

    se actualizó algoritmo con función

--- ValorMedio.py
+++ ValorMedio.py
@@ -4,12 +4,10 @@ Created on Tue Sep 21 07:33:28 2021
 @author: Daniel
 """
 
-
 import numpy as np
 import skimage
 
 def operacion_bandas(color_ban, ban_pan):
-
     return (color_ban + ban_pan) * 0.5
 
 im_pan = skimage.io.imread('pan8r.tif', plugin='tifffile')
@@ -18,17 +16,17 @@ n = int(im_multi.shape[2])
 listaunion = []
 pan_float = im_pan.astype(np.float32)
 
-print("Se sacarán " + str(n) + " bandas")
+def combinar():
+    i=0
+    while i < n:
+        matrix = im_multi[:,:,i]
+        matrixfloat = matrix.astype(np.float32)
+        fusionbandas = operacion_bandas (matrixfloat, pan_float)
+        union = fusionbandas.astype(np.uint8)
+        listaunion.append(union)
+        i=i+1
+    return listaunion
 
-i=0
-while i < n:
-    matrix = im_multi[:,:,i]
-    matrixfloat = matrix.astype(np.float32)
-    fusionbandas = operacion_bandas (matrixfloat, pan_float)
-    union = fusionbandas.astype(np.uint8)
-    listaunion.append(union)
-    i=i+1
-
-i = i-1
+combinar()
 fusioned_image = np.stack((listaunion),axis = 2)
-t = skimage.io.imsave('prueba_ciclo.tif',fusioned_image, plugin='tifffile')
+t = skimage.io.imsave('prueba_funcion.tif',fusioned_image, plugin ='tifffile')
